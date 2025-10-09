25d848d95c94c050bd2cdcd780bd7c611df66e09
Jaini Shah <123638815+JainiShah-24@users.noreply.github.com>
2025-10-09 17:45:37 +0530

    Fix ApplicationsSection import error

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -9,6 +9,7 @@ import LeaseAssetsSection from './LeaseAssetsSection';
 import NewsSection from './NewsSection';
 import JobListingsSection from './JobListingsSection';
 import NotificationsSection from './NotificationsSection';
+import ApplicationsSection from './ApplicationsSection';
 
 const Dashboard: React.FC = () => {
   const { user } = useApp();
